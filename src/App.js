d0a703a436df24bf3f27d30284a5dd87b8e9fc03
Partha Sarathi Dutta <coder.partha08@gmail.com>
2026-06-14 23:52:13 +0600

    Movie-Nest Added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,105 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const tempMovieData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt0133093",
+    Title: "The Matrix",
+    Year: "1999",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SX300.jpg",
+  },
+  {
+    imdbID: "tt6751668",
+    Title: "Parasite",
+    Year: "2019",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BYWZjMjk3ZTItODQ2ZC00NTY5LWE0ZDYtZTI3MjcwN2Q5NTVkXkEyXkFqcGdeQXVyODk4OTc3MTY@._V1_SX300.jpg",
+  },
+];
+
+const tempWatchedData = [
+  {
+    imdbID: "tt1375666",
+    Title: "Inception",
+    Year: "2010",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_SX300.jpg",
+    runtime: 148,
+    imdbRating: 8.8,
+    userRating: 10,
+  },
+  {
+    imdbID: "tt0088763",
+    Title: "Back to the Future",
+    Year: "1985",
+    Poster:
+      "https://m.media-amazon.com/images/M/MV5BZmU0M2Y1OGUtZjIxNi00ZjBkLTg1MjgtOWIyNThiZWIwYjRiXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_SX300.jpg",
+    runtime: 116,
+    imdbRating: 8.5,
+    userRating: 9,
+  },
+];
+
+
+
+export default function App(){
+
+  const [isOpen1 , setOpen1]= useState(true);
+  const [isOpen2 , setOpen2] = useState(true);
+  const [movies , setMovies] = useState(tempMovieData);
+
+
+
+  return(
+    <div>
+        <nav className="nav-bar">
+          <div className="logo">
+            <span className="logo">       
+              🎬 
+            </span>
+            <h1>movieNest</h1>
+          </div>
+          <input type="text" className="search" placeholder="Search Movie..."  />
+          <p className="num-results">
+            Found <strong>3</strong> results
+          </p>
+        </nav>
+      <main className="main" >
+        <div className="box">
+          <button className="btn-toggle" onClick={() => setOpen1((open)=> !open)} >
+               {isOpen1 ? '-':'+'}
+          </button>
+          {isOpen1 && (
+            <ul className="list">
+              {movies.map((movie , index) => (
+                <li  key={index} >
+                  <h3>{movie.Title}</h3>
+                  <img src={movie.Poster} alt={movie.Title} />
+                  <p>📆 {movie.Year}</p>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+        <div className="box">
+
+            <button  className="btn-toggle" onClick={()=> setOpen2((open) => !open)} >
+              {isOpen2 ? '-':'+'}
+            </button>
+            {isOpen2 && (
+                
+            )}
+        </div>
+      </main>
     </div>
-  );
+  )
 }
 
-export default App;
+
